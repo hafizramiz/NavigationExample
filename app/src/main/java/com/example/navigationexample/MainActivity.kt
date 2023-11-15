4671e76243c0a9841660a6db670112e315e789dd
Hafiz Ramiz <ramizovhafiz@gmail.com>
2023-11-15 13:59:29 +0300

    first commit

---
+++ app/src/main/java/com/example/navigationexample/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.navigationexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigationexample.ui.theme.NavigationExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+
+                ) {
+                    HomePage()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage() {
+    TextField(value = "", onValueChange = {})
+}
---
+++ app/src/main/java/com/example/navigationexample/ui/theme/Deneme.kt
@@ -0,0 +1,9 @@
+package com.example.navigationexample.ui.theme
+
+//fun main() {
+//    val result = buildString {
+//        append("Merhaba, ")
+//        append("Dünya!")
+//    }
+//    println(result)
+//}
